d8563bf92d851aef4d3a2069787772f90c1ee9dc
HarunakunZR <raflyanthonydms@gmail.com>
2026-03-31 17:46:44 +0700

    soal Nomor 1 dan Gambar

---
+++ ReverseTheNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c, d, e;
+
+    cout << "Masukkan 5 angka: ";
+    cin >> a >> b >> c >> d >> e;
+
+    cout << "Hasil dibalik: ";
+    cout << e << " " << d << " " << c << " " << b << " " << a << endl;
+
+    return 0;
+}
